5f857751c5aa848a62e6e8aef466d5558e568d86
FutureDevelopersuz <107317980+BoBuRKhaN7475@users.noreply.github.com>
2022-08-18 10:27:36 +0500

    mini_project

---
+++ testing/testing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testing
+//
+//  Created by Apple on 30/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .position(x: 50, y: 20)
+        for i in 2...5 {
+            return
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
